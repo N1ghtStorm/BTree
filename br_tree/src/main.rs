ee83ca9a8a29d3c1a971e26de0526ef7c0aa7df1
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-12 23:58:38 +0300

    add impl

--- br_tree/src/main.rs
+++ br_tree/src/main.rs
@@ -11,16 +11,30 @@ struct BNodei32 {
 }
 
 struct BTree {
-    pub root_node: Option<BNodei32>
+    pub root_node: Option<Box<BNodei32>>
 }
 
 impl BTree {
     pub fn add_value(&self, value: i32) {
+        let root_node_opt = &self.root_node;
+        let root_value = match root_node_opt {
+            None => panic!("what what what??"),
+            Some(asd) => asd.value
+        };
+
+        let uuu = match root_value > value {
+            true => {},
+            false => {println!("asdsadad")}
+        };
+
+    }
+
+    fn compare_node_and_value(node: BNodei32, ) {
 
     }
 
     pub fn new(initial_value: i32) -> Self {
-        let root_node = BNodei32 { value: initial_value, left: None, right: None };
+        let root_node = Box::new(BNodei32 { value: initial_value, left: None, right: None }) ;
         BTree {root_node: Some(root_node)}
     }
 }
