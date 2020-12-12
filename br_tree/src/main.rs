96bf046e2aff0a00088045d8d23749b24c7f42b0
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-12 23:33:45 +0300

    add node and btree

--- br_tree/src/main.rs
+++ br_tree/src/main.rs
@@ -1,9 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let b_tree = BTree::new(95);
+
+
 }
 
+struct BNodei32 {
+    pub value: i32,
+    pub left: Option<Box<BNodei32>>,
+    pub right: Option<Box<BNodei32>>
+}
 
-// struct BTree {
-//     Right: BTree,
-//     Left: BTree
-// }
+struct BTree {
+    pub root_node: Option<BNodei32>
+}
+
+impl BTree {
+    pub fn add_value(&self, value: i32) {
+
+    }
+
+    pub fn new(initial_value: i32) -> Self {
+        let root_node = BNodei32 { value: initial_value, left: None, right: None };
+        BTree {root_node: Some(root_node)}
+    }
+}
